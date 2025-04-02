44f2c5316cd726ac3812e80df59dc49f08953fff
pandey88himanshu <pandey88himanshu@gmail.com>
2025-04-02 23:06:11 +0530

    removed vite

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+import User from "../models/user.model.js";
+
+exports.signup = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <div className='text-red-500 text-xl font-bold'>App</div>;
-};
-
-export default App;
