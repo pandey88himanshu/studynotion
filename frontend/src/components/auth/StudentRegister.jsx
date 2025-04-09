e82c1d5227dafe4785a0d03f13d97aabe619ccf2
pandey88himanshu <pandey88himanshu@gmail.com>
2025-04-09 21:38:52 +0530

    env added and some cleanups

--- frontend/src/components/auth/StudentRegister.jsx
+++ frontend/src/components/auth/StudentRegister.jsx
@@ -3,7 +3,7 @@ import ImageComponent from "./ImageComponent";
 
 const StudentRegister = () => {
   return (
-    <section className='bg-[#121212] text-white w-full min-h-screen  flex  justify-center '>
+    <section className='bg-[#121212] text-white w-full h-[86vh]  flex items-center  justify-center '>
       <div className='max-w-[1280px]  mx-auto  text-[#AFB2BF] flex justify-between  w-full pt-36 '>
         <div className=''> left </div>
         <div>
